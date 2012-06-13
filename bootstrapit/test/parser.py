00069e69da534ed66cd171d7dac419dee0c7d080
krozark <maxime.barbier1991@gmail.com>
2012-06-13 18:59:47 -0400

    add base str to parse

--- bootstrapit/test/parser.py
+++ bootstrapit/test/parser.py
@@ -4,28 +4,30 @@ content = """// Variables.less
 // Variables to customize the look and feel of Bootstrap
 // -----------------------------------------------------
 
-
-
-// GLOBAL VALUES
-// --------------------------------------------------
-
-
-// Grays
+// Type Test
 // -------------------------
 @black:                 #000;
-@grayDarker:            #222;
-@grayDark:              #333;
-@gray:                  #555;
-@grayLight:             #999;
-@grayLighter:           #eee;
-@white:                 #fff;
+@bodyBackground:        @white;
+@linkColorHover:        darken(@linkColor, 15%);
+@sansFontFamily:        "Helvetica Neue", Helvetica, Arial, sans-serif;
+@baseFontSize:          13px;
+@baseFontFamily:        @sansFontFamily;
+@zindexDropdown:          1000;
+@iconSpritePath:          "../img/glyphicons-halflings.png";
+//@fluidGridColumnWidth:    6.382978723%;
+@fluidGridColumnWidth:      1% * ((@gridColumnWidth * 100) / @gridRowWidth);
 """
 
 class TYPE:
     Color=1
     Pix=2
     Percent=3
-    Txt=4
+    Nombre=4
+    Txt=5
+    Img=6
+    Reference=7
+    Calulate=8
+
     Unknow=404
 
 class var:
