8a1cc92051b82697f1d45c56cd4d76cdbe5aa7ec
krozark <maxime.barbier1991@gmail.com>
2012-06-13 20:23:34 -0400

    update caode

--- bootstrapit/test/parser.py
+++ bootstrapit/test/parser.py
@@ -6,16 +6,17 @@ content = """// Variables.less
 
 // Type Test
 // -------------------------
-@black:                 #000;
-@bodyBackground:        @white;
-@linkColorHover:        darken(@linkColor, 15%);
-@sansFontFamily:        "Helvetica Neue", Helvetica, Arial, sans-serif;
-@baseFontSize:          13px;
-@baseFontFamily:        @sansFontFamily;
-@zindexDropdown:          1000;
-@iconSpritePath:          "../img/glyphicons-halflings.png";
 //@fluidGridColumnWidth:    6.382978723%;
+@iconSpritePath:          "../img/glyphicons-halflings.png";
+@sansFontFamily:        "Helvetica Neue", Helvetica, Arial, sans-serif;
 @fluidGridColumnWidth:      1% * ((@gridColumnWidth * 100) / @gridRowWidth);
+@linkColorHover:        darken(@linkColor, 15%);
+@bodyBackground:        @white;
+@baseFontFamily:        @sansFontFamily;
+@black:                 #000;
+@baseFontSize:          13px;
+//@fluidGridColumnWidth:    6.382978723%;
+@zindexDropdown:          1000;
 """
 
 class TYPE:
@@ -24,7 +25,7 @@ class TYPE:
     Percent=3
     Nombre=4
     Txt=5
-    Img=6
+    File=6
     Reference=7
     Calulate=8
 
@@ -32,12 +33,66 @@ class TYPE:
 
 class var:
     name =""
+    app = ""
     type = TYPE.Unknow
     value =0
     
 
-var = {}
+def ComType(txt):
+    size = len(txt)
+    if txt[-1] == ';'
+        return None
+    if size >=5:
+        if txt[].islower():
+            return "desc"
+        return "app"
+    return None
 
-for lines in content.split('\n'):
-    pass
+def TxtType(txt):
+    if '../' in txt:
+        return TYPE.File
+    return TYPE.Txt
+
+import re
+def tofloat(txt):
+    i = ''.join(re.findall(r'[\d.]+',txt))
+    if i:
+        return float(i)
+    return None
+
+var = []
+last_big_title =""
+last_small_title = ""
+last_des = ""
+for line in content.split('\n'):
+    if len(txt)< 5:
+        continue
+
+    if line.startswith('//'):
+        type = ComType(line)
+
+    if '"' in txt or "'" in txt:
+        TxtType(line)
+
+    if any('(' in line,'+' in line,'*' in line,'-' in line,' / ' in line):
+        #calculate to ignore
+        continue
+
+    if '@' in line:
+        continue
+
+    if '#' in line:
+        continue
+
+    if 'px' in line:
+        continue
+
+    if "%" in line:
+        continue
+
+
+
+
+
+
 
