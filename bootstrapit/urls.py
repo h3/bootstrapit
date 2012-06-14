3a19f3cfec4c5e80c606d46fca73cb58d2810403
Krozark <maxime.barbier1991@gmail.com>
2012-06-14 12:19:11 -0400

    up urls

--- bootstrapit/urls.py
+++ bootstrapit/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import *
 from bootstrapit.views import *
 
 urlpatterns=patterns('',
-    url(r'^$',          HomeView.as_view(),     name='bootstrapit-home'),
+    url(r'^all$',          HomeView.as_view(),     name='bootstrapit-home'),
+    url(r'v/(?P<slug>[\d\w-\.]+)',  Version.as_view(),      name="by-version'),
 )
--- bootstrapit/views.py
+++ bootstrapit/views.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, TemplateView, DetailView, BaseView
 from bootstrapit.models import *
 
 
 class HomeView(TemplateView):
     template_name = 'bootstrapit/home.html'
+
+class Version(BaseView):
+    
+    def render_to_response(self, context, **response_kwargs):
+        response_kwargs['content_type'] = 'application/json'
+        return HTTPResponse(
+                'test'
+                )
